0bb101c1153102013f89e5da677d76af468e09e3
rvmn <roberto.vanmaanen@gmail.com>
2014-08-14 18:58:18 +0200

    major rebuild

--- install.sh
+++ install.sh
@@ -2,25 +2,25 @@
 rm -rf Dockerfile && rm -rf dind && apt-get install curl wget && wget https://rawgit.com/rvmn/docker-dev-cloud9/master/Dockerfile && wget https://rawgit.com/rvmn/docker-dev-cloud9/master/dind
 ( grep 'dcruns' ~/.bashrc | wc -l ; )>0 || echo 'dcruns(){ docker run -d -v $(pwd):/workspace -p "$3":"$3" docker-dev --username "$1" --password "$2"; }' >> ~/.bashrc
 if [ ! -z "$1" ] && [ ! -z "$2" ] && [ ! -z "$3" ]; then
-echo '\
+echo '
 EXPOSE '$3'
 ENTRYPOINT ["forever", "/cloud9/server.js", "-w", "/workspace", "-l", "0.0.0.0"]' >> Dockerfile
 grep -q "dcrun()" ~/.bashrc && sed "s/dcrun()/dcrun(){ dcruns $1 $2 $3; }/" -i ~/.bashrc || sed "$ a\dcrun(){ dcruns $1 $2 $3; }" -i ~/.bashrc
 fi
 if [ ! -z "$1" ] && [ ! -z "$2" ] && [ -z "$3" ]; then
-echo '\
+echo '
 EXPOSE 1337
 ENTRYPOINT ["forever", "/cloud9/server.js", "-w", "/workspace", "-l", "0.0.0.0"]'  >> Dockerfile
 grep -q "dcrun()" ~/.bashrc && sed "s/dcrun()/dcrun(){ dcruns $1 $2 3000; }/" -i ~/.bashrc || sed "$ a\dcrun(){ dcruns $1 $2 3000; }" -i ~/.bashrc
 fi
 if [ ! -z "$1" ] && [ -z "$2" ]; then
-echo '\
+echo '
 EXPOSE '$1'
 ENTRYPOINT ["forever", "/cloud9/server.js", "-w", "/workspace", "-l", "0.0.0.0"]' >> Dockerfile
 grep -q "dcrun()" ~/.bashrc && sed "s/dcrun()/dcrun(){ dcruns dvr pass $1; }/" -i ~/.bashrc || sed "$ a\dcrun(){ dcruns dvr pass $1; }" -i ~/.bashrc
 fi
 if [ -z "$1" ]; then
-echo '\
+echo '
 EXPOSE 1337
 ENTRYPOINT ["forever", "/cloud9/server.js", "-w", "/workspace", "-l", "0.0.0.0"]' >> Dockerfile
 grep -q "dcrun()" ~/.bashrc && sed "s/dcrun()/dcrun(){ dcruns c9dev pass 3000; }/" -i ~/.bashrc || sed "$ a\dcrun(){ dcruns c9dev pass 3000; }" -i ~/.bashrc
