e3676ac78bdc427340f36e1e75cdbbd0094ac344
rvmn <roberto.vanmaanen@gmail.com>
2014-08-14 06:39:36 +0200

    major rebuild

--- install.sh
+++ install.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
-cd ~ && rm -rf Dockerfile && wget https://rawgit.com/rvmn/docker-dev-cloud9/master/Dockerfile && wget https://rawgit.com/rvmn/docker-dev-cloud9/master/dind
-curl -fsSL https://rawgit.com/rvmn/docker-dev-cloud9/master/docker-alias >> ~/.bashrc && source ~/.bashrc
+cd ~ && rm -rf Dockerfile && rm -rf dind && wget https://rawgit.com/rvmn/docker-dev-cloud9/master/Dockerfile && wget https://rawgit.com/rvmn/docker-dev-cloud9/master/dind
 if [ ! -z "$1" ]&&[! -z "$2" ]&&[! -z "$3" ]; then
-echo $( cat Dockerfile ; echo 'EXPOSE '$3'\nCMD ["forever", "/cloud9/server.js", "-w", "/workspace", "-l", "0.0.0.0","--username '$1'","--password '$2'"]' ) > Dockerfile
+echo 'EXPOSE '$3'
+CMD ["forever", "/cloud9/server.js", "-w", "/workspace", "-l", "0.0.0.0","--username '$1'","--password '$2'"]' >> Dockerfile
 fi
 if [ ! -z "$1" ]&&[! -z "$2" ]&&[ -z "$3" ]; then
-echo $( cat Dockerfile ; echo 'EXPOSE 3030\nCMD ["forever", "/cloud9/server.js", "-w", "/workspace", "-l", "0.0.0.0","--username '$1'","--password '$2'"]' ) > Dockerfile
+echo 'EXPOSE 3030
+CMD ["forever", "/cloud9/server.js", "-w", "/workspace", "-l", "0.0.0.0","--username '$1'","--password '$2'"]'  >> Dockerfile
 fi
 if [ ! -z "$1" ]&&[ -z "$2" ]; then
-echo $( cat Dockerfile ; echo 'EXPOSE '$1'\nCMD ["forever", "/cloud9/server.js", "-w", "/workspace", "-l", "0.0.0.0","--username c9dev","--password pass"]' ) > Dockerfile
+echo 'EXPOSE '$1'
+CMD ["forever", "/cloud9/server.js", "-w", "/workspace", "-l", "0.0.0.0","--username c9dev","--password pass"]' >> Dockerfile
 fi
 if [ -z "$1" ]; then
-echo $( cat Dockerfile ; echo 'EXPOSE 3030\nCMD ["forever", "/cloud9/server.js", "-w", "/workspace", "-l", "0.0.0.0","--username c9dev","--password pass"]' ) > Dockerfile
+echo 'EXPOSE 3030
+CMD ["forever", "/cloud9/server.js", "-w", "/workspace", "-l", "0.0.0.0","--username c9dev","--password pass"]'  >> Dockerfile
 fi
 docker build -t docker-dev .
 docker run -d -P docker-dev .
