1d4e67bcfa1f6c7bc0d2c57e385baf615393778d
rvmn <roberto.vanmaanen@gmail.com>
2014-08-14 18:24:00 +0200

    major rebuild

--- install.sh
+++ install.sh
@@ -2,19 +2,19 @@
 rm -rf Dockerfile && rm -rf dind && apt-get install curl wget && wget https://rawgit.com/rvmn/docker-dev-cloud9/master/Dockerfile && wget https://rawgit.com/rvmn/docker-dev-cloud9/master/dind
 ( grep 'dcruns' ~/.bashrc | wc -l ; )>0 || echo 'dcruns(){ docker run -d -v $(pwd):/workspace -p "$3":"$3" docker-dev --username "$1" --password "$2"; }' >> ~/.bashrc
 if [ ! -z "$1" ] && [ ! -z "$2" ] && [ ! -z "$3" ]; then
-echo 'EXPOSE 1337
+echo 'EXPOSE '$3'
 ENTRYPOINT ["forever", "/cloud9/server.js", "-w", "/workspace", "-l", "0.0.0.0"]' >> Dockerfile
-grep -q "dcrun()" ~/.bashrc && sed "s/dcrun()/dcrun(){ dcruns '$1' '$2' '$3'; }/" -i ~/.bashrc || sed "$ a\dcrun(){ dcruns '$1' '$2' '$3'; }" -i ~/.bashrc
+grep -q "dcrun()" ~/.bashrc && sed "s/dcrun()/dcrun(){ dcruns $1 $2 $3; }/" -i ~/.bashrc || sed "$ a\dcrun(){ dcruns $1 $2 $3; }" -i ~/.bashrc
 fi
 if [! -z "$1" ] && [! -z "$2" ] && [ -z "$3" ]; then
 echo 'EXPOSE 1337
 ENTRYPOINT ["forever", "/cloud9/server.js", "-w", "/workspace", "-l", "0.0.0.0"]'  >> Dockerfile
-grep -q "dcrun()" ~/.bashrc && sed "s/dcrun()/dcrun(){ dcruns '$1' '$2' 3000; }/" -i ~/.bashrc || sed "$ a\dcrun(){ dcruns '$1' '$2' 3000; }" -i ~/.bashrc
+grep -q "dcrun()" ~/.bashrc && sed "s/dcrun()/dcrun(){ dcruns $1 $2 3000; }/" -i ~/.bashrc || sed "$ a\dcrun(){ dcruns $1 $2 3000; }" -i ~/.bashrc
 fi
 if [! -z "$1" ] && [ -z "$2" ]; then
-echo 'EXPOSE 1337
+echo 'EXPOSE '$1'
 ENTRYPOINT ["forever", "/cloud9/server.js", "-w", "/workspace", "-l", "0.0.0.0"]' >> Dockerfile
-grep -q "dcrun()" ~/.bashrc && sed "s/dcrun()/dcrun(){ dcruns dvr pass '$1'; }/" -i ~/.bashrc || sed "$ a\dcrun(){ dcruns dvr pass '$1'; }" -i ~/.bashrc
+grep -q "dcrun()" ~/.bashrc && sed "s/dcrun()/dcrun(){ dcruns dvr pass $1; }/" -i ~/.bashrc || sed "$ a\dcrun(){ dcruns dvr pass $1; }" -i ~/.bashrc
 fi
 if [ -z "$1" ]; then
 echo 'EXPOSE 1337
@@ -23,8 +23,6 @@ grep -q "dcrun()" ~/.bashrc && sed "s/dcrun()/dcrun(){ dcruns c9dev pass 3000; }
 source ~/.bashrc
 docker build -t docker-dev .
 # postinstall
-rm -rf Dockerfile
-rm -rf dind
 ( grep '#c9dev docker aliases' ~/.bashrc | wc -l ; )>0 || curl -fsSL https://rawgit.com/rvmn/docker-dev-cloud9/master/docker-alias >> ~/.bashrc && source ~/.bashrc
 wget https://rawgit.com/rvmn/docker-dev-cloud9/master/QuickStart.md
 wget https://rawgit.com/rvmn/docker-dev-cloud9/master/metbp.sh
@@ -32,5 +30,11 @@ wget https://rawgit.com/rvmn/docker-dev-cloud9/master/README.md
 mkdir meteor-apps
 mkdir rails-apps
 wget https://rawgit.com/rvmn/docker-dev-cloud9/master/rails-install.sh && chmod +x rails-install.sh
-rm -rf install.sh
+# keep install scripts for reviewing / continue install
+rm -rf dind
+#rm -rf Dockerfile
+#rm -rf install.sh
+echo '
+Done!! fingers crossed all is installed well
+'
 exit
