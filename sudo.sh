f8cda19f2420e8542aa2bcd27bb8af965fa5bd2d
rvmn <roberto.vanmaanen@gmail.com>
2014-08-14 08:53:29 +0200

    major rebuild

--- sudo.sh
+++ sudo.sh
@@ -1,6 +1,6 @@
 if [ -z "$1" ]; then
   echo "Starting up visudo with this script as first parameter" 
-  export EDITOR=$0 && sudo -E visudo 
+  export EDITOR=$0 && visudo 
 else
   echo "Changing sudoers" 
   echo "c9dev ALL=(root) NOPASSWD:/cloud9/*" >> $1
