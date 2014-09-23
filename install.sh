17510a94dcb8c44c972fbf2f5aef15715ebf838e
rvmn <rvmn@users.noreply.github.com>
2014-09-23 21:17:13 +0200

    small fix

--- install.sh
+++ install.sh
@@ -12,18 +12,18 @@ cat Dockerfile
 cat <<EOF  
 ---------------------------------------------------------------------------------------------
 
-      			 Time to ENTER or wait, for 7 seconds or ..
+      			 Check the Dockerfile ^^^ and press ENTER
 
 ---------------------------------------------------------------------------------------------
 EOF
 read -t 7
 
-#build!
-docker build -t docker-c9 .
-
-# add aliases to bashrc of host
+# add aliases to bashrc of host system
 [ -z $( grep '#c9dev docker aliases' ~/.bashrc) ] && curl -fsSL https://rawgit.com/rvmn/docker-dev-cloud9/master/docker-alias.sh >> ~/.bashrc && source ~/.bashrc
 brcadd dcset 'brcadd dcrun "docker run --privileged -d -v $(pwd):/workspace -p 3000:3000 -p 4000:4000 -p 5000:5000 -p 3131:3131 $3 $4 docker-c9 --username $1 --password $2"'
+
+#build!
+docker build -t docker-c9 .
 
 # postinstall clean
 rm -rf Dockerfile && rm -rf install.sh
