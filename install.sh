17c9170d4f0674cfca76ff44324476e380db411b
rvmn <rvmn@users.noreply.github.com>
2014-12-27 17:38:01 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-rm -rf Dockerfile && apt-get install curl wget && wget https://rawgit.com/rvmn/docker-dev-cloud9/master/Dockerfile
+rm -rf Dockerfile && apt-get install curl wget && wget https://raw.githubusercontent.com/rvmn/docker-dev-cloud9/master/Dockerfile
 [ ! -z $1 ] && echo "ENV MONGO_URL $1" >> Dockerfile
 cat <<EOF  
 ---------------------------------------------------------------------------------------------
@@ -19,7 +19,7 @@ EOF
 read -t 7
 
 # add aliases to bashrc of host system
-[ -z $( grep '#c9dev docker aliases' ~/.bashrc) ] && curl -fsSL https://rawgit.com/rvmn/docker-dev-cloud9/master/docker-alias.sh >> ~/.bashrc && source ~/.bashrc
+[ -z $( grep '#c9dev docker aliases' ~/.bashrc) ] && curl -fsSL https://raw.githubusercontent.com/rvmn/docker-dev-cloud9/master/docker-alias.sh >> ~/.bashrc && source ~/.bashrc
 brcadd dcset 'brcadd dcrun "docker run --privileged -d -v $(pwd):/workspace -p 3000:3000 -p 4000:4000 -p 5000:5000 -p 3131:3131 $3 $4 docker-c9 --username $1 --password $2"'
 
 #build!
