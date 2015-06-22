68bb196d7e8d88385190385a18e1ca032f6bacc8
rvmn <rvmn@users.noreply.github.com>
2015-06-22 22:44:34 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-rm -rf Dockerfile && apt-get install curl wget && wget https://raw.githubusercontent.com/rvmn/docker-dev-cloud9/master/Dockerfile
+rm -rf docker-dev-cloud9 && apt-get install -y curl wget git && git clone https://raw.githubusercontent.com/rvmn/docker-dev-cloud9 && cd docker-dev-cloud9
 [ ! -z $1 ] && echo "ENV MONGO_URL $1" >> Dockerfile
 cat <<EOF  
 ---------------------------------------------------------------------------------------------
@@ -19,13 +19,13 @@ EOF
 read -t 7
 
 # add aliases to bashrc of host system
-[ -z $( grep '#c9dev docker aliases' ~/.bashrc) ] && curl -fsSL https://raw.githubusercontent.com/rvmn/docker-dev-cloud9/master/docker-alias.sh >> ~/.bashrc && source ~/.bashrc
-brcadd dcset 'brcadd dcrun "docker run --privileged -d -v $(pwd):/workspace -p 3000:3000 -p 4000:4000 -p 5000:5000 -p 8181:8181 $3 $4 docker-c9 --username $1 --password $2"'
+[ -z $( grep '#docker aliases' ~/.bashrc) ] && curl -fsSL https://raw.githubusercontent.com/rvmn/docker-dev-cloud9/master/docker-alias.sh >> ~/.bashrc && source ~/.bashrc
+brcadd dcset 'brcadd dcrun "docker run --privileged -d -v $(pwd):/workspace -p 3000:3000 -p 4000:4000 -p 5000:5000 -p 8181:8181 -p 6080:6080 $3 $4 cloud9 --username $1 --password $2"'
 
 #build!
-docker build -t docker-c9 .
+docker build -t cloud9 .
 
 # postinstall clean
-rm -rf Dockerfile && rm -rf install.sh
-echo 'Done!! Hopefully all went good, first run: dcset <user> <name> MONGO_URL=<mongolink>, then start with dcrun, if not installed try rerunning all or remove the image and rerun'
+cd .. && rm -rf docker-dev-cloud9
+echo "Done!! Hopefully all went good, to set username and password: 'dcset <user> <name>', then start with 'dcrun'"
 exit
